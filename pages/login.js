4769ee4b473a0dc6b0aca8c26b5fa39d92b4a54a
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2022-01-25 11:25:31 -0800

    Initial commit

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <p>Login</p>
+    </div>
+  );
+}
